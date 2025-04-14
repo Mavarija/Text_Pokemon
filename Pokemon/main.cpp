5f9cc67683562dfa72ee50ee22968b05d48c8b51
Vaiva Marija <marija.vaiva@gmail.com>
2025-04-14 15:09:06 +0100

    player name

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+using namespace std;
 
 int main() {
 
-    // Test 2
+    string player_name;
+    cout << "Enter your name \n";
+    cin >> player_name;
+    cout << "Great Start, " << player_name << ", looks like you have understood the main() function properly now!" << endl;
 
     return 0;
 }
